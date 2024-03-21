6095ef38addb2f4058c23a71dd25fde8e24c6cf6
Anna83 <Ceca1283@gmail.com>
2024-03-21 17:00:38 -0400

    done

---
+++ src/Svetlana/Week1_solutions/Numbers_DivideWithoutOperator.java
@@ -0,0 +1,8 @@
+package Svetlana.Week1_solutions;
+
+public class Numbers_DivideWithoutOperator {
+
+     /*
+    Write a method that can divide two numbers without using division operator
+     */
+}
