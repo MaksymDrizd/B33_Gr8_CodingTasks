1f6ec690c4679cb11973b9b45136f95033ffa8dc
kamal <kamalmezali00@gmail.com>
2024-03-27 03:27:52 -0500

    my souloution of task Numbers_OddOrEven

--- src/Kamal/Week01_Solutions/Numbers_OddOrEven.java
+++ src/Kamal/Week01_Solutions/Numbers_OddOrEven.java
@@ -1,4 +1,21 @@
 package Kamal.Week01_Solutions;
 
 public class Numbers_OddOrEven {
+    public static void main(String[] args) {
+        int number = 5;
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+    }
 }
+
+ /*
+    Write  a method which can identifies given number is even or odd
+        EX:
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
+
+     */
