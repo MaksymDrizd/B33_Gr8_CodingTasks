00b246ca856115425b111278a4be84f493b0ef24
smoha <smohamed.fares22@gmail.com>
2024-03-28 15:31:36 -0400

    tasks result

--- src/Mohamed/Numbers_DivideWithoutOperator.java
+++ src/Mohamed/Numbers_DivideWithoutOperator.java
@@ -2,4 +2,18 @@ package Mohamed;
 
 public class Numbers_DivideWithoutOperator {
 
+     /*
+    Write a method that can divide two numbers without using division operator
+     */
+     public static void main(String[] args) {
+         int a = 10;
+         int b = 5;
+         System.out.println(divide(a, b));
+
+     }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
 }
--- src/Mohamed/Numbers_OddOrEven.java
+++ src/Mohamed/Numbers_OddOrEven.java
@@ -1,4 +1,42 @@
 package Mohamed;
 
 public class Numbers_OddOrEven {
+
+   /*
+            Write  a method which can identifies given number is even or odd
+            EX:
+            identify(5) ->  "Odd"
+            identify(6) ->  "Even"
+
+        */
+
+    public static void main(String[] args) {
+
+        boolean result = isEven(6);
+        System.out.println("the number is Event = " + result);
+        result = isOdd(5);
+        System.out.println("the number is Odd = " + result);
+
+
+
+    }
+
+    public static boolean isOdd(int number) {
+        if (number % 2 == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
 }
