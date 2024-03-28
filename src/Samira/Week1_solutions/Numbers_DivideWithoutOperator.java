fe31b50a7ba104ca9bdc2394b9e7fa7d794ddaab
samira firman <146880710+Firman-samira@users.noreply.github.com>
2024-03-28 13:11:47 -0400

    Tasks solutions

--- src/Samira/Week1_solutions/Numbers_DivideWithoutOperator.java
+++ src/Samira/Week1_solutions/Numbers_DivideWithoutOperator.java
@@ -2,4 +2,8 @@ package Samira.Week1_solutions;
 
 public class Numbers_DivideWithoutOperator {
 
+     /*
+    Write a method that can divide two numbers without using division operator
+     */
+
 }
