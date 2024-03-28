e005258c09a1b860cdee712f6df724c81753fc67
MaksymDr <makc2708@gmail.com>
2024-03-28 10:53:20 -0400

    tasks completed

--- src/Max/week01_solutions/Numbers_DivideWithoutOperator.java
+++ src/Max/week01_solutions/Numbers_DivideWithoutOperator.java
@@ -3,8 +3,26 @@ package Max.week01_solutions;
 public class Numbers_DivideWithoutOperator {
 
     public static void main(String[] args) {
+        int num1 = 16;
+        int num2 = 4;
+        System.out.println(num1 +" / " + num2 + " = " + division(num1, num2));
 
 
     }
+    public static int division(int num1, int num2){
+        int count = 0;
+        if (num2 == 0){
+            System.err.println("CANNOT BE DIVIDED BY ZERO");
+            System.exit(1);
+        }
+        while (num1 >= num2){
+            num1 -= num2;
+            count++;
+        }
+        return count;
+    }
 
+    /*
+    Write a method that can divide two numbers without using division operator
+     */
 }
--- src/Max/week01_solutions/Numbers_OddOrEven.java
+++ src/Max/week01_solutions/Numbers_OddOrEven.java
@@ -4,6 +4,19 @@ public class Numbers_OddOrEven {
 
     public static void main(String[] args) {
 
+        int n = 6;
+        evenOrOdd(n);
 
+
+    }
+    /*
+    Write  a method which can identifies given number is even or odd
+        EX:
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
+     */
+    public static void evenOrOdd(int n){
+        String evenOrOdd = (n % 2 == 0) ? "Even":"Odd";
+        System.out.println(("Number "+ n + " is " + evenOrOdd));
     }
 }
