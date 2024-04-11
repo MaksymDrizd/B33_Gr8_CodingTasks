784cec71bcc2d7827aff07f65d47e7357c859db0
MaksymDrizd <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>
2024-04-11 17:04:37 -0400

    week03 tasks ready

--- src/Max/week03_solutions/Number_Prime.java
+++ src/Max/week03_solutions/Number_Prime.java
@@ -7,11 +7,14 @@ public class Number_Prime {
         Write a method that can check if a number is prime or not
      */
     public static void main(String[] args) {
-        int num = 3;
+        int num = 7;
         System.out.println(isPrime(num));
 
     }
     public static boolean isPrime(int num){
+        if (num <= 1){
+            return false;
+        }
         for (int i = 2; i < num; i++) {
             if (num % i == 0)
             return false;
