dd67519afe2725b3207511ce52db1e23c9846240
Anna83 <ceca1283@gmail.com>
2024-04-11 19:41:34 -0400

    task changed

--- src/Svetlana/week3_tasks/Number_ReverseInteger.java
+++ src/Svetlana/week3_tasks/Number_ReverseInteger.java
@@ -2,23 +2,24 @@ package Svetlana.week3_tasks;
 
 public class Number_ReverseInteger {
 
-/*
-Write a return method that can reverse
-negative number and return it as int
- */
-public static void main(String[] args) {
-    int num = -124;
-    System.out.println(reverseNegativeNum(num));
-}
 
-    public static int reverseNegativeNum(int num){
-        int result = Math.abs(num);
-        return result;
+
+
+    public static int reverseInteger(int num) {
+        int reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        return reversed;
     }
 
-
-
+    public static void main(String[] args) {
+        System.out.println(reverseInteger(-123));  // Output: -321
+    }
 }
+
 
 
 
