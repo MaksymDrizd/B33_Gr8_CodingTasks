96d13e22bd1d77fbdfc18a55b3109de96a55fbe4
MaksymDrizd <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>
2024-05-02 14:21:06 -0400

    tasks solution week06

---
+++ src/Max/week06_solutions/String_SumOfAllIntegers.java
@@ -0,0 +1,21 @@
+package Max.week06_solutions;
+
+public class String_SumOfAllIntegers {
+
+
+
+    /*
+    return the sum of all integers found in the parameter String
+
+            Please implement this method to return the sum of all integers found in the
+            parameter String. You can assume that integers are separated from other
+            parts with one or more spaces (' ' symbol).
+
+            For example, s="12 some text 3 7", result: 22 (12+3+7=22)
+        */
+
+
+
+
+
+}
