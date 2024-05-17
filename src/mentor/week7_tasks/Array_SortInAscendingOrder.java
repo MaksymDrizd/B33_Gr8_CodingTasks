93f18d4af48b9b1f98dc762983f88240362193c8
TroyYC <troy@cydeo.com>
2024-05-17 14:34:44 -0400

    week 7 tasks shared

---
+++ src/mentor/week7_tasks/Array_SortInAscendingOrder.java
@@ -0,0 +1,14 @@
+package mentor.week7_tasks;
+
+public class Array_SortInAscendingOrder {
+
+    /*
+    Write a method that can sort an int array in Ascending order without using the sort method
+     */
+
+
+
+
+
+
+}
---
+++ src/mentor/week7_tasks/Array_SortInDescendingOrder.java
@@ -0,0 +1,13 @@
+package mentor.week7_tasks;
+
+public class Array_SortInDescendingOrder {
+
+    /*
+        Write a return method that can sort an int array in descending order without using the sort method
+     */
+
+
+
+
+
+}
