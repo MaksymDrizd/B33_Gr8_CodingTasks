b5ca47577d97e16629c3890d79edafec48bf3e2f
TroyYC <troy@cydeo.com>
2024-06-08 14:10:04 -0400

    week 10 tasks shared

---
+++ src/mentor/week10_tasks/List_Sort_Desc.java
@@ -0,0 +1,13 @@
+package mentor.week10_tasks;
+
+public class List_Sort_Desc {
+
+    /*
+ArrayList - sorting in descending order
+Write a method that can sort the ArrayList in descending order without
+using the sort method.
+     */
+
+
+
+}
---
+++ src/mentor/week10_tasks/Map_SortByValues.java
@@ -0,0 +1,13 @@
+package mentor.week10_tasks;
+
+public class Map_SortByValues {
+
+    /*
+        Write a method that can sort the map by values
+     */
+
+
+
+
+
+}
