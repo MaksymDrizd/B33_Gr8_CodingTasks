d9c4c27ff4dc62312d8b6b48e74dd8c49c6b2e19
TroyYC <troy@cydeo.com>
2024-06-18 18:25:21 -0400

    week 11 tasks shared

---
+++ src/mentor/week11_tasks/Map_FrequencyOfCharacters.java
@@ -0,0 +1,18 @@
+package mentor.week11_tasks;
+
+public class Map_FrequencyOfCharacters {
+
+    /*
+    Write a method that prints the frequency of each character from a String
+     */
+
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
---
+++ src/mentor/week11_tasks/Map_MinValue.java
@@ -0,0 +1,36 @@
+package mentor.week11_tasks;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Map_MinValue {
+
+    /*
+    Write a method that can return the minimum value from a map (DO NOT use sort method)
+     */
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("k0", -1);
+        map.put("k5", 5);
+        map.put("k2", 2);
+        map.put("k1", -3);
+        map.put("k3", 3);
+
+//        System.out.println(minValue(map));
+//
+//        System.out.println(minimum(map));
+
+    }
+
+}
